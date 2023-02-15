da4f0228fffba6556604efda67ee498b4bba564b
Chance <jchancehud@gmail.com>
2023-02-14 18:11:07 -0600

    chore: check that input array is not mutated

--- contracts/Test.sol
+++ contracts/Test.sol
@@ -9,9 +9,12 @@ interface _PoseidonT3 {
 
 contract Test {
   function benchmark(address p) public view returns (uint) {
+    uint[2] memory input = [uint(2), 1];
     uint g = gasleft();
-    uint r = _PoseidonT3(p).poseidon([uint(2), 1]);
+    uint r = _PoseidonT3(p).poseidon(input);
     console.log(g-gasleft());
+    require(input[0] == 2, 'memcorrupt');
+    require(input[1] == 1, 'memcorrupt');
     return r;
   }
 }
